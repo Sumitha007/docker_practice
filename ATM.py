2010eac016d5cdef45ec14a5bddfbd79b2ad3519
Sumitha007 <jsumitha012@gmail.com>
2026-09-17 14:50:53 +0530

    Refactor ATM.py to remove user input and simplify deposit/withdrawal logic

--- ATM.py
+++ ATM.py
@@ -2,34 +2,22 @@ balance = 10000
 
 print("===== ATM =====")
 
-while True:
-    print("\n1. Check Balance")
-    print("2. Deposit")
-    print("3. Withdraw")
-    print("4. Exit")
+print("Initial Balance:", balance)
 
-    choice =1 
+# Deposit
+amount = 5000
+balance += amount
+print("Amount deposited:", amount)
 
-    if choice == "1":
-        print("Balance:", balance)
+# Withdraw
+amount = 2000
 
-    elif choice == "2":
-        amount = int(input("Enter deposit amount: "))
-        balance += amount
-        print("Amount deposited successfully.")
+if amount <= balance:
+    balance -= amount
+    print("Amount withdrawn:", amount)
+else:
+    print("Insufficient balance.")
 
-    elif choice == "3":
-        amount = int(input("Enter withdrawal amount: "))
+print("Final Balance:", balance)
 
-        if amount <= balance:
-            balance -= amount
-            print("Please collect your cash.")
-        else:
-            print("Insufficient balance.")
-
-    elif choice == "4":
-        print("Thank you!")
-        break
-
-    else:
-        print("Invalid choice.")
+print("ATM process completed.")
